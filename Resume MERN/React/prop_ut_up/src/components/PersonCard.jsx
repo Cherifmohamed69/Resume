7312686be52cc13cb3d0a601d430dcebffa9bbdf
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-12-20 11:33:20 +0100

    prop it up

--- Resume MERN/React/prop_ut_up/src/components/PersonCard.jsx
+++ Resume MERN/React/prop_ut_up/src/components/PersonCard.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import style from './card.css';
 
 class PersonCard extends Component {
     render() {
-        return <div>
+        return <div className='card'>
     <h1> {this.props.first_name}, {this.props.last_name}</h1>
     <br/>age :{this.props.age} <br/> hair color : {this.props.hair_color}
 </div>;
+
     }
 }
-    
+
 export default PersonCard;
