13e4afc7bf3647dddf50c907790ec179950a2de3
Cherifmohamed69 <coachcherifmohamed@gmail.com>
2023-12-20 14:58:17 +0100

    increment

--- Resume MERN/React/prop_ut_up/src/App.js
+++ Resume MERN/React/prop_ut_up/src/App.js
@@ -1,18 +1,60 @@
+import React, { useState } from "react";
 import PersonCard from "./components/PersonCard";
 
 function App() {
-  const all_users =[
-    {"first_name":"Doe","last_name":"jane ","age":"45", "hair_color":"black"},
-    {"first_name":"Smith","last_name":"john ", "age":"88","hair_color": "brown"},
-    {"first_name":"fillmore","last_name":"millard ", "age":"50","hair_color": "brown"},
-    {"first_name":"smith","last_name":"maria ", "age":"62","hair_color": "brown"}
-]
-    
-    return (
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      first_name: "Doe",
+      last_name: "Jane",
+      age: 45,
+      hair_color: "black",
+    },
+    {
+      id: 2,
+      first_name: "Smith",
+      last_name: "John",
+      age: 88,
+      hair_color: "brown",
+    },
+    {
+      id: 3,
+      first_name: "Fillmore",
+      last_name: "Millard",
+      age: 50,
+      hair_color: "brown",
+    },
+    {
+      id: 4,
+      first_name: "Smith",
+      last_name: "Maria",
+      age: 62,
+      hair_color: "brown",
+    },
+  ]);
+
+  const handleIncrement = (userId) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId ? { ...user, age: user.age + 1 } : user
+      )
+    );
+  };
+
+  return (
     <div className="App">
-      {all_users.map(i => {return <PersonCard first_name={i.first_name} last_name={i.last_name} age={i.age} hair_color={i.hair_color}  />
-      })}
-     </div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <PersonCard
+            first_name={user.first_name}
+            last_name={user.last_name}
+            age={user.age}
+            handleIncrement={() => handleIncrement(user.id)}
+            hair_color={user.hair_color}
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
--- Resume MERN/React/prop_ut_up/src/components/PersonCard.jsx
+++ Resume MERN/React/prop_ut_up/src/components/PersonCard.jsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './card.css';
 
 class PersonCard extends Component {
-    render() {
-        return <div className='card'>
-    <h1> {this.props.first_name}, {this.props.last_name}</h1>
-    <br/>age :{this.props.age} <br/> hair color : {this.props.hair_color}
-</div>;
+  render() {
+    const { first_name, last_name, age, hair_color, handleIncrement } = this.props;
 
-    }
+    return (
+      <div className='card'>
+        <h1>{first_name}, {last_name}</h1>
+        <br/>Age: {age} <button onClick={handleIncrement}>
+          Increment Age
+        </button>
+        
+        <br /> Hair Color: {hair_color}
+      </div>
+    );
+  }
 }
 
 export default PersonCard;
